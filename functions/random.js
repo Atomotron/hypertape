60b40dea0aa2a251124abb98f8e1b4157077ba32
user <user@example.com>
2022-12-18 21:31:32 -0600

    Improved templating

--- functions/random.js
+++ functions/random.js
@@ -1,5 +1,28 @@
+function template(posts,submission=null) {
+    const submissionReceived = submission ? 
+    `<div id="submission_received">Your submission has been received. (${Date()})</div>` : '';
+    return `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1> Hypertape </h1>
+<form action="" method="get" class="form-example">
+    <label for="submission"></label>
+    <input type="text" name="submission" id="submission" required>
+    <input type="submit" value="Post">
+</form>
+    ${submissionReceived}
+    <div class='post'>${posts.join('</div>\n<div class='post'>')}</div>
+</body>
+</html>`;
+}
+
 export async function onRequestGet(context) {
-    const blob = await context.env.HYPERTAPE_KV.get("hypertape-blob");
-    return new Response(`Requested ${Date()}
-${blob}`);
+    const posts = JSON.parse(
+        await context.env.HYPERTAPE_KV.get("hypertape-blob")
+    );
+    const submission = false;
+    return new Response(template(posts,submission));
 }
