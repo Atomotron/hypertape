a222e36117540c8c5c0e37916097771c5d1d8646
user <user@example.com>
2022-12-18 21:13:56 -0600

    async main function

--- functions/random.js
+++ functions/random.js
@@ -1,4 +1,4 @@
-export function onRequest(context) {
+export async function onRequest(context) {
     return new Response(`Requested ${Date()}.
 ${JSON.parse(await HYPERTAPE_KV.get("hypertape-blob")).join('\n')}
 `);
